2ec553037fad574ea5e48b992a7735cb0c5f68ff
surendraseepala <seepalasurendra@gmail.com>
2023-08-28 18:54:09 +0530

    calsi to local_repo

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,11 +1,54 @@
-// Question -1
-
-function multiply(a, b){
-    return a*b;
+/*function printResult(){
+    let textBoxValue = document.getElementById('aVal').value;
+    document.getElementById('result').textContent = textBoxValue;
 }
 
-// output => multiply(2, 4) => 8
 
-// Question -2
+function printValtoDisplay(value){
+    document.getElementById('displayPanel').textContent += value;
+}*/
+let display = document.getElementById('displayPanel');
 
-// solution
+let buttons = Array.from(document.getElementsByClassName('btn'));
+
+buttons.map( btn => {
+    btn.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '<=':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case '%':
+                    display.innerText +='%';
+                    break;
+            case '/':
+                display.innerText +='/';
+                break;
+
+            case 'X':
+            display.innerText +='*';
+            break;
+
+            case '-':
+                display.innerText +='-';
+            break;
+
+            case '+':
+                display.innerText +='+';
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
